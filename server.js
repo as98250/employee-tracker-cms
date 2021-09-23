4fb3dd9b38c3a5e29f91cba600c9edf35fa4c9a7
Ariell SCott <scott.ariell00@gmail.com>
2021-09-23 03:34:14 -0400

    created functions on server to initiate app and chose from the start menu

--- server.js
+++ server.js
@@ -15,14 +15,96 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_db database.`)
 );
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+initiateApplication();
+
+const initiateApplication = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'begin',
+            message: "Please select an action to begin",
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add An Employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+
+    ]).then((response) => {
+        switch (response.begin) {
+            case 'View All Departments':
+                viewDep();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'View All Employees':
+                viewEmployees();
+                break;
+            case 'Add an Employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployee();
+                break;
+            case 'Exit':
+                console.log('You have exited the application');
+                break;
+        }
+    }).catch((err) => {
+        console.log(err);
+        initiateApplication();
+    })
+
+};
+
+function viewDept() {
+    db.query(`SELECT * FROM department`, (err, results) => {
+        if (err) {
+            console.log(err);
+        } res.json();
+    });
+}
+
+
+function viewRoles() {
+    db.query(`SELECT * FROM role`, (err, results) => {
+        if (err) {
+            console.log(err);
+        } res.json();
+    });
+}
+
+function viewEmployees() {
+    db.query(`SELECT * FROM employee`, (err, results) => {
+        if (err) {
+            console.log(err);
+        } res.json();
+    });
+}
+
+function addEmployee() {
+    
+}
+
+function updateEmployee() {
+
+}
+
+
+
+db.query('SELECT * FROM employee_db', function (err, results) {
+    if (err) return console.log(err);
+    res.json();
+});
 
 app.use((req, res) => {
     res.status(404).end();
-  });
-  
-  app.listen(PORT, () => {
+});
+
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
+});
